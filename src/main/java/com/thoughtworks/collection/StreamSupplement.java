7c5f33ff677cea572701b1ec3b7dad99194883d6
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-01 14:36:30 +0800

    implement StreamSupplement

--- src/main/java/com/thoughtworks/collection/StreamSupplement.java
+++ src/main/java/com/thoughtworks/collection/StreamSupplement.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.collection;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class StreamSupplement {
 
@@ -10,15 +13,15 @@ public class StreamSupplement {
     }
 
     public List<Integer> sortFromMaxToMin(List<Integer> numbers) {
-        return null;
+        return numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public double getAverage(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().mapToDouble(Integer::doubleValue).average().orElse(0.0);
     }
 
     public int getMaxValue(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().max(Comparator.naturalOrder()).orElse(0);
     }
 
 }
