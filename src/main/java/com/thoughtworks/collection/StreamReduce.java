68abb98871249bca5fe75cf07da0eb0a1624250b
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-01 14:10:37 +0800

    implement StreamReduce functions

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -1,18 +1,19 @@
 package com.thoughtworks.collection;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number % 2 != 0).mapToInt(Integer::intValue).reduce((prev, curr) -> curr).orElse(numbers.get(0));
     }
 
     public String getLongest(List<String> words) {
-        return null;
+        return words.stream().max(Comparator.comparing(String::length)).orElse(words.get(0));
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream().mapToInt(String::length).reduce(0, Integer::sum);
     }
 }
