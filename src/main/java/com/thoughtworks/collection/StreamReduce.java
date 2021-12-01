941e1946e93cf434697fce968b759ec5f23de2bc
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-01 14:37:06 +0800

    updated StreamReduce orElse to return null or 0 after feedback

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return numbers.stream().filter(number -> number % 2 != 0).mapToInt(Integer::intValue).reduce((prev, curr) -> curr).orElse(numbers.get(0));
+        return numbers.stream().filter(number -> number % 2 != 0).mapToInt(Integer::intValue).reduce((prev, curr) -> curr).orElse(0);
     }
 
     public String getLongest(List<String> words) {
-        return words.stream().max(Comparator.comparing(String::length)).orElse(words.get(0));
+        return words.stream().max(Comparator.comparing(String::length)).orElse(null);
     }
 
     public int getTotalLength(List<String> words) {
